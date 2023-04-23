96f08775ef1c93c5f1be32a4f4f0c87bb26fd160
slipstream20 <yoyforever@gmail.com>
2023-04-23 09:45:37 +0100

    hello

--- config/backup.php
+++ config/backup.php
@@ -110,7 +110,7 @@ return [
             /*
              * The filename prefix used for the backup zip file.
              */
-            'filename_prefix' => 'PSGENERALDRUGCENTRE-',
+            'filename_prefix' => '',
 
             /*
              * The disk names on which the backups will be stored.
