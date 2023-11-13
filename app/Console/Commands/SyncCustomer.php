50531e4240911e551cf9cb5fce9c7221f085c45f
Yusuf Olatunji <yoyforever@gmail.com>
2023-11-13 14:42:45 +0100

    some

---
+++ app/Console/Commands/SyncCustomer.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Customer;
+use Illuminate\Console\Command;
+
+class SyncCustomer extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sync:customer';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->info('Gathering Customer Data');
+        $customers = Customer::where('status',1);
+        $chunk_numbers = round(($customers->count() / 200));
+        $customers->chunk(200,function($customer) use (&$chunk_numbers){
+            $all_data = [];
+            foreach($customer as $cus){
+                $all_data[] = $cus->getBulkPushData();
+            }
+            $postdata = ['table'=>'existing_customer','data'=> $all_data];
+            $this->info('Parsing Customer Data Complete');
+            $this->info('Posting Customer Data to '.onlineBase());
+            $response = _POST('sync_customer',$postdata);
+
+            if($response['status'] == true){
+                $chunk_numbers = $chunk_numbers-1;
+                $this->info('Customer data has been posted successfully '.$chunk_numbers);
+                sleep(4);
+            }
+        });
+
+        return Command::SUCCESS;
+    }
+}
